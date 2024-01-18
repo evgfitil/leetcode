4afe2794fffc89b981d0401a948d2eacefd04f51
Evgenii Akhmetzianov <eraakhmetzyanov@sberbank.ru>
2024-01-18 08:39:25 +0300

    optimize solution for 1762

--- 04_stack/d50_buildings_with_a_ocean_view.go
+++ 04_stack/d50_buildings_with_a_ocean_view.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func findBuildings(heights []int) []int {
 	var stack []int
-	for right := len(heights) - 1; right >= 0; right-- {
-		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[right] {
+	for right := 0; right < len(heights); right++ {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] <= heights[right] {
 			stack = stack[:len(stack)-1]
 		}
 		stack = append(stack, right)
@@ -14,8 +14,8 @@ func findBuildings(heights []int) []int {
 }
 
 func main() {
-	//testBuildings := []int{4, 2, 3, 1} // -> 0 2 3
-	testBuildings := []int{4, 3, 2, 1} // -> 0 1 2 3
+	testBuildings := []int{4, 2, 3, 1} // -> 0 2 3
+	//testBuildings := []int{4, 3, 2, 1} // -> 0 1 2 3
 	//testBuildings := []int{1, 3, 2, 4} // -> 3
 	fmt.Println(findBuildings(testBuildings))
 }
